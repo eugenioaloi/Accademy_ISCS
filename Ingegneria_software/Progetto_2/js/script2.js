92087b58194cae83bdb0be342a4f1db6ec72499a
EugenioAloi <60814811+eugenioaloi@users.noreply.github.com>
2023-03-21 16:21:49 +0100

    Another day, another $
    new commit

--- Ingegneria_software/Progetto_2/js/script2.js
+++ Ingegneria_software/Progetto_2/js/script2.js
@@ -1,11 +1,9 @@
-//stamp un saluto all'utente nel p #benvenuto
+/*
 var nomeUt= "paperino";
 var benvenuto = document.getElementById("benvenuto");
 benvenuto.innerHTML = "ciao " + nomeUt;
 
-//voglio creare un elenco di film con relativa durata e mostrarli nella pagina nel div #elenco
-var film= ["Fight club","Eternal sunshine of spotless mind","The godfather"];
-var durata = [150,180,200,230];
+//stamp un saluto all'utente nel p #benvenuto
 
 var elenco = document.getElementById("elenco");
 for(i = 0;i<film.length;i++ ){
@@ -20,6 +18,28 @@ for(i = 0;i<durata.length;i++ ){
 }
 
 somma.innerHTML = "<p>"+  sommaMinuti + "</p>" ;
+*/
 
+//Acquisizione nome utente
+var username = prompt('Ciao utente, digita il tuo username');
+
+var benvenuto = document.getElementById("benvenuto");
+benvenuto.innerHTML="Ciao " + username + " ecco i film in sala";
+
+//Mostra elenco film con relativa durata
+var film= ["Fight club","Eternal sunshine of spotless mind","The godfather"];
+var durata = [150,180,200,230];
+
+var elenco = document.getElementById("elenco");
+var sommaMinuti = document.getElementById("sommaMinuti")
+
+var minutiTot = 0;
+
+for(let i=0;i<film.length;i++){
+    elenco.innerHTML += "<li>" + film[i] + "-" + durata[i] + " min </li";
+    minutiTot += durata[i] ;
+}
+
+sommaMinuti.innerHTML = "<p> Minuti totali: "+  minutiTot + "</p>";
 
 
