4c36d784613d41da22f9ddc07b6191c8ff2b1320
EugenioAloi <60814811+eugenioaloi@users.noreply.github.com>
2023-04-28 17:18:39 +0200

    new commit

---
+++ SP_FORMAZIONE/ws-java/primoProgetto/src/checkString/CheckString.java
@@ -0,0 +1,33 @@
+package checkString;
+
+import java.util.Scanner;
+
+public class CheckString {
+	public static void main(String[] args) {
+		String s = "123 ciao 0.2";
+		
+		System.out.println(checkString(s));
+	}
+
+	private static boolean checkString(String s) {
+		
+		Scanner sc = new Scanner(s);
+		
+		while (sc.hasNext()) {
+			if(!sc.hasNextInt()) {
+				return false;
+			}
+			if(!sc.hasNext()) {
+				return false;
+			}
+			if(!sc.hasNextDouble()) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+
+}
